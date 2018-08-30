325170c80dd7101b8f7ee4fc8a4d17a463d3de34
Ruchika-Chakala <40537407+Ruchika-Chakala@users.noreply.github.com>
2018-08-30 19:56:09 +0530

    Update testNetwork.sh

--- lib/testNetwork.sh
+++ lib/testNetwork.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #Spin up the truffle docker image & mount the truffle test scripts inside docker 
-printf "\033[0;33mTruffle contract deployment & execution of truffle tests are about to begin\033[0m\n"
+printf "\033[0;33mTruffle contract deployment & execution of truffle tests are about to begin...\033[0m\n"
 docker run -it -v $(pwd):/home  -w /home/testcases syneblock/cicd:quorum-maker-testsuite ./truffleChange.sh
